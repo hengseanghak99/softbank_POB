5e910244bcadfb5e922e5e7319a09f6da6096836
hengseanghak99 <hengseanghak10499@gmail.com>
2024-06-20 16:50:04 +0700

    change some code

--- sb-pob/pages/Functions_Check/TopPage.spec.js
+++ sb-pob/pages/Functions_Check/TopPage.spec.js
@@ -1,19 +1,12 @@
 import { login_logout_action } from "../../component/login_logout_actions";
 const { test, expect } = require("@playwright/test");
 const { login_success } = login_logout_action;
-require('dotenv').config();
-
-console.log('Environment Variables:', {
-  EMAIL: process.env.EMAIL,
-  PASSWORD: process.env.PASSWORD,
-  TENANTID: process.env.TENANTID,
-});
-
 
 test("validate Top page [ Check page can be collapsed or expanded side bar ]", async ({ page }) => {
   await login_success(page);
 
   try {
+    console.log(process.env.EMAIL);
     await expect(page.locator("aside")).toHaveClass("sb-sidebar sb-sidebar-sm");
     // If the above assertion passes, collapse the sidebar
     await page.locator('div').filter({ hasText: '折りたたむ' }).nth(2).click();
